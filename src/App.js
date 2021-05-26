ba56928541f8f430a81c0011d634174fe2586e7f
dakotafabro <dakotafabrocgu@gmail.com>
2021-05-26 10:52:09 -0700

    Added notes component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import NotesList from "./NotesList";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from Notes</h1>
+
+      <NotesList />
     </div>
   );
 }
---
+++ src/NotesList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Note from "./Note";
+
+export default function NotesList() {
+  return (
+    <div className="NotesList">
+      <div>
+        <Note />
+      </div>
+    </div>
+  );
+}
