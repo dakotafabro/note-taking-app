f1f0a6c440f640dd164cb3e0c2f022f96140c27f
dakotafabro <dakotafabrocgu@gmail.com>
2021-06-07 15:53:13 -0700

    Added footer and final touches

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="Footer fixed-bottom m-3">
+    <div className="Footer fixed-bottom ">
       Coded by <a href="https://dakotafabro.netlify.app/">Dakota Fabro</a> in
       partnership with{" "}
       <a href="https://www.youtube.com/channel/UC-Zcse8tC53G34Uo4kzLeAg">
